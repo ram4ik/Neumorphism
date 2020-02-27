31c1db2c9eace8e7fab543fceb5fc60d06851442
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-27 10:21:27 +0200

    SimpleButtonStyle

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -8,9 +8,55 @@
 
 import SwiftUI
 
+extension Color {
+    static let offWhite = Color(red: 255/255, green: 255/255, blue: 235/255)
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .contentShape(Circle())
+            .background(
+                Group {
+                    if configuration.isPressed {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                    } else {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                    }
+                }
+            )
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Neumorphism")
+        ZStack {
+            Color.offWhite
+            
+            VStack(spacing: 30) {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.offWhite)
+                    .frame(width: 300, height: 300)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                
+                Button(action: {
+                    print("Button tapped")
+                }) {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.gray)
+                }
+                .buttonStyle(SimpleButtonStyle())
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
