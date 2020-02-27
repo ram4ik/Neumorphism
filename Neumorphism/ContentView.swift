a169a72f972bb1b9e30ff02d510e43781ac5583f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-27 11:43:10 +0200

    ColorfullBackground

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -9,9 +9,16 @@
 import SwiftUI
 
 extension Color {
+    
     static let offWhite = Color(red: 225/255, green: 225/255, blue: 235/255)
+    
+    //
     static let darkStart = Color(red: 50/255, green: 60/255, blue: 65/255)
     static let darkEnd = Color(red: 25/255, green: 25/255, blue: 30/255)
+    
+    //
+    static let lightStart = Color(red: 60/255, green: 160/255, blue: 240/255)
+    static let lightEnd = Color(red: 30/255, green: 80/255, blue: 120/255)
 }
 
 extension LinearGradient {
@@ -80,6 +87,32 @@ struct DarkBackground<S: Shape>: View {
     }
 }
 
+//
+struct ColorfullBackground<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+    
+    var body: some View {
+        ZStack {
+            if isHighlighted {
+                shape
+                    //.fill(Color.darkEnd)
+                    .fill(LinearGradient(Color.lightEnd, Color.lightStart))
+                    .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
+                    .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
+            } else {
+                shape
+                    //.fill(Color.darkEnd)
+                    .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                    .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
+                    .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
+            }
+        }
+    }
+}
+
 struct DarkButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
@@ -103,6 +136,34 @@ struct DarkToggleStyle: ToggleStyle {
         }
         .background(
             DarkBackground(isHighlighted: configuration.isOn, shape: Circle())
+        )
+    }
+}
+
+//
+struct ColorfullButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .contentShape(Circle())
+            .background(
+                ColorfullBackground(isHighlighted: configuration.isPressed, shape: Circle())
+            )
+            .animation(nil)
+    }
+}
+
+struct ColorfullToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            configuration.label
+                .padding(30)
+                .contentShape(Circle())
+        }
+        .background(
+            ColorfullBackground(isHighlighted: configuration.isOn, shape: Circle())
         )
     }
 }
@@ -135,6 +196,7 @@ struct ContentView: View {
                 // TODO: step 2 (uncomment)
                 .buttonStyle(SimpleButtonStyle())
                 //.buttonStyle(DarkButtonStyle())
+                //.buttonStyle(ColorfullButtonStyle())
                 
                 
                 // TODO: step 4 (uncomment)
@@ -144,6 +206,9 @@ struct ContentView: View {
 //                        .foregroundColor(.white)
 //                }
 //                .toggleStyle(DarkToggleStyle())
+                    
+//                    //or
+//                .toggleStyle(ColorfullToggleStyle())
             }
         }
         .edgesIgnoringSafeArea(.all)
